c9da328c8f7eac29e1ef7d718d12cb591e6b3c86
Pito Salas <pitosalas@gmail.com>
2012-05-24 13:16:03 -0400

    remove sqlite verson number

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 group :development, :test do
-  gem 'sqlite3', '1.3.5'
+#  gem 'sqlite3', '1.3.5'
+  gem 'sqlite3'
   gem 'rspec-rails', '2.9.0'
 end
 # Gems used only for assets and not required 
