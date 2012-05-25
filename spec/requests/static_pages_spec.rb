656fe8ae9500b7d494fbb2b959c5a43f8a6e4f11
Pito Salas <pitosalas@gmail.com>
2012-05-25 11:19:29 -0400

    end of chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,30 @@
 require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                  :text => "Ruby on Rails Tutorial Sample App | Home")
+      :text => "Ruby on Rails Tutorial Sample App")
     end
-  end
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end  
+    describe "Help page" do
+      it "should have the content 'Help'" do
+        visit '/static_pages/help'
+        page.should have_content('Help')
+      end
     end
-  end
-  describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
+    describe "About page" do
+      it "should have the content 'About Us'" do
+        visit '/static_pages/about'
+        page.should have_content('About Us')
+      end
     end
   end
 end
