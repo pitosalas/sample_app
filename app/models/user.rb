a37bfb12ba0679a77562b07da34c3a76dc529515
Pito Salas <pitosalas@gmail.com>
2012-06-10 16:21:55 -0400

    end-chapter-6-328-again

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,20 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
   validates :name, presence: true, length: { maximum: 50 }
+  
   validates :email, presence: true
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :email, presence: true, 
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+  
   validates :password, presence: true, length: { minimum: 6 }
+
+  validates :password_confirmation, presence: true
+  
   before_save { |user| user.email = email.downcase }
+  
   has_secure_password   
 end
